df2658e0765ae37072f0b03c604d7f0444e27c05
malikbhai1984 <139610065+malikbhai1984@users.noreply.github.com>
2025-11-26 13:21:57 +0500

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'; // automatically loads .env
+import 'dotenv/config'; // local testing only
 import { Client, GatewayIntentBits } from 'discord.js';
 
 const client = new Client({
@@ -6,18 +6,5 @@ const client = new Client({
 });
 
 const TOKEN = process.env.TOKEN;
-const API_FOOTBALL = process.env.API_FOOTBALL;
-const PORT = process.env.PORT || 3000;
-
-client.once('ready', () => {
-    console.log(`Bot is online! Logged in as ${client.user.tag}`);
-    console.log(`API_FOOTBALL key loaded:`, !!API_FOOTBALL);
-});
-
-client.on('messageCreate', (message) => {
-    if(message.content === '!ping') {
-        message.reply('Pong!');
-    }
-});
-
+console.log("TOKEN length:", TOKEN?.length); // check loading
 client.login(TOKEN);
